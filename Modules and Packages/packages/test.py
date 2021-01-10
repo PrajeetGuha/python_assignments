6d6732bbc62a0ecf0e449e673fce788ed701b9e7
Prajeet Guha <58776463+PrajeetGuha@users.noreply.github.com>
2021-01-10 20:18:20 +0530

    Add files via upload

---
+++ Modules and Packages/packages/test.py
@@ -0,0 +1,16 @@
+from animals import amphibian,bird,fish,mammal,reptile
+
+amphibian.examples(5)
+bird.examples(4)
+fish.examples(7)
+mammal.characteristics()
+reptile.characteristics()
+
+from patterns import pattern
+
+n = int(input('Number of rows:'))
+pattern.pattern1(n)
+pattern.pattern2(n)
+pattern.pattern3(n)
+pattern.pattern4(n)
+pattern.pattern5(n)
